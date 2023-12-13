bad3bc9b2626b28e0549191438f23c8f589a1adf
Rajatsparke <96182123+Rajatsparke@users.noreply.github.com>
2023-12-13 15:33:08 +0000

    update

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import gspread
+cred_file = "key.json"
+gc = gspread.service_account(cred_file)
+
+sh = gc.open("ROI Calculator - Institutes")
+wks = sh.get_worksheet(0)
+institutes = wks.col_values(1)
+fee = wks.col_values(2)
+url = f"https://docs.google.com/spreadsheets/d/1WxFPOD3SNEwPoiqBOyUG_5wG41QVwpfU3pdjjEgonq0/gviz/tq?tqx=out:csv&sheet=Sheet1"
+df = pd.read_csv(url)
+#st.write(df)
+
+
 
 # Title
-st.title('ROI Calculator - BASL vs Trad MBA')
+#st.title('Calculate your ROI \n **Kraftshala\'s Business and Sales Launchpad (BASL) or Traditional MBA**')
+st.set_page_config(page_title='Calculate your ROI ',page_icon="bar_chart")
+st.title('Calculate your ROI \n **Kraftshala\'s Business and Sales Launchpad (BASL) or Traditional MBA**')
 
-# Sliders
-basl_investment = st.number_input('BASL-Investment/Fee in Rupees', value = 160000)
-mba_fee = st.number_input('Traditional MBA Fee in Rupees', value = 1000000)
+basl_investment = st.number_input('How much BASL costs:', value = 175000)
+#mba_fee = st.number_input('Traditional MBA Fee in Rupees', value = 1000000)
+#mba_institute = st.selectbox("Select your Institute",options=institutes)
+#mba_fee = float(st.selectbox('Your Institute\'s fee ',options=fee))
+column1_options = df['Institute'].tolist()
+selected_option = st.selectbox("How much a Tradional MBA costs? Select an Institute to find out", column1_options)
 
-# Input
-basl_salary = st.number_input('BASL minimum salary after placement (6 months)', value=900000)
-mba_salary = st.number_input("MBA Salary after placement (2 years)", value = 1500000)
+#corresponding value = institute's fees
+corresponding_value = df.loc[df['Institute'] == selected_option, 'Total Fees (Rs)'].values
+if len(corresponding_value) > 0:
+     st.success(f"Fee for {selected_option} is : Rs {corresponding_value[0]}")
+else:
+     st.warning("No corresponding value found.")
+
+#st.write(corresponding_value/12)
+
+
+basl_salary = st.number_input('The minimum salary you get placed at if you opt for BASL (in 6 months)', value=900000)
+mba_salary = st.number_input("The average salary you get placed at if you opt for traditional MBA (in 24 months)", value = 1500000)
 
 annual_growth = st.number_input("Annual growth in %age",value = 15)
 
@@ -20,41 +47,73 @@ basl_monthly_salary = basl_salary/12
 #st.write('BASL Minimum monthly salary after 6 months is Rs.',basl_monthly_salary)
 
 #n = st.slider("Years from today",0,5,1)
-a = st.slider("Months since placement",-6,23,1)
+a = st.slider("Months since placement",-6,30,1)
 
 #if n == 0:
  #   st.write("Your approx. salary per month will be", basl_salary/12)
  #   st.write("Therefore, ROI after 6 months from today will be",round(((basl_salary/12)/basl_investment)*100),'%')
 b = 0
 c = basl_salary/12*(12)
+
 e = (c+((basl_salary*(1+annual_growth/100))/12*(a%12)))
 roi_basl = 0
 roi_mba = 0
 if 1<=a<13:
     b = basl_salary/12*(a%13)
-    st.write("Congratulations on your placement. Your have approx. earned", basl_salary/12*(a%13))
-    st.write("Therefore, your ROI in BASL is",(round((b/basl_investment)*100)),'%')
+    st.write(" *Opted for BASL:* Congratulations on your placement! \n Your have approx. earned Rs.", basl_salary/12*(a%13))
+    
     roi_basl = (round((b/basl_investment)*100))
-    st.write("ROI of MBA is 0 because it is of 2 years")
+    st.write(" *Opted for MBA :* You're still in training")
+    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round((b/basl_investment)*100)),'% vs 0')
 elif a <  1:
-    st.write("You are still under training")
+    st.write("You are in training")
     roi = 0
 elif 13<=a<=18:
     d = (basl_salary*(1+annual_growth/100))/12
-    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is now Rs.",basl_monthly_salary*(1+annual_growth/100))
+    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is Rs.",round(basl_monthly_salary*(1+annual_growth/100)))
     st.write("Your total earnings after",a ,'months via BASL is',(c+(d*(a%12))))
     e = (c+(d*(a%12)))
-    st.write("Therefore, your ROI in BASL is",(round(((c+(d*(a%12)))/basl_investment)*100)),'%')
+    st.write(" *Opted for MBA :* You're still in training")
+    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round(((c+(d*(a%12)))/basl_investment)*100)),'% vs 0')
     roi_basl = (round(((c+(d*(a%12)))/basl_investment)*100))
-    st.write("ROI of MBA is 0 because it is of 2 years")
+   
 elif 18<a<24:
     d = (basl_salary*(1+annual_growth/100))/12
-    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is now Rs.",basl_monthly_salary*(1+annual_growth/100))
-    st.write("Your total earnings after",a ,'months is',(c+(d*(a%12))))
+    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is Rs.",round(basl_monthly_salary*(1+annual_growth/100)))
+    st.write("Your total earnings after",a ,'months via BASL is',(c+(d*(a%12))))
     e = (c+(d*(a%12)))
-    st.write("Therefore, your ROI in BASL is",(round(((c+(d*(a%12)))/basl_investment)*100)),'%')
     roi_basl = (round(((c+(d*(a%12)))/basl_investment)*100))
-    st.write("You got placed via MBA and your monthly salary is",mba_salary/12)
+    st.write("You got placed via MBA and your monthly salary is", mba_salary/12)
     st.write("You total earnings till now via an MBA are",(mba_salary/12)*(a%18))
-    roi_mba = round((((mba_salary/12)*(a%18))/mba_fee)*100)
+    roi_mba = ((((mba_salary/12)*(a%18))/corresponding_value)*100)
+    roii_mba = round(float(roi_mba))
+    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round(((c+(d*(a%12)))/basl_investment)*100)),'% vs',roii_mba,'%')
+elif a==24:
+    d = (basl_salary*(1+annual_growth/100))/12
+    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is Rs.",round(basl_monthly_salary*(1+annual_growth/100)))
+    st.write("Your total earnings after",a ,'months via BASL is',round((c+(d*(12)))))
+    
+    roi_basl = (round(((c+(d*(12)))/basl_investment)*100))
+    st.write("You got placed via MBA and your monthly salary is", mba_salary/12)
+    st.write("You total earnings till now via an MBA are",round((mba_salary/12)*(a%18)))
+    roi_mba = ((((mba_salary/12)*(a%18))/corresponding_value)*100)
+    roii_mba = round(float(roi_mba))
+    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round(((c+(d*(12)))/basl_investment)*100)),'% vs',roii_mba,'%')
+elif 25<=a<=30:
+    d = (basl_salary*(1+annual_growth/100))/12
+    st.write("You received a",annual_growth,"% increment again and hence your new monthly salary is Rs.",round(d*(1+annual_growth/100)))
+    f = (d*(1+annual_growth/100))
+    x = round((c+(d*(12))))
+    st.write("Your total earnings after",a ,'months via BASL is',round(x+(f*(a%12))))
+    
+    roi_basl = ((x+(f*(a%12))/basl_investment)*100)
+    st.write("You got placed via MBA and your monthly salary is", mba_salary/12)
+    st.write("You total earnings till now via an MBA are",round((mba_salary/12)*(a%18)))
+    roi_mba = ((((mba_salary/12)*(a%18))/corresponding_value)*100)
+    roii_mba = round(float(roi_mba))
+    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',round(((x+(f*(a%12)))/basl_investment)*100),'% vs',roii_mba,'%')
+
+
+
+    
 
