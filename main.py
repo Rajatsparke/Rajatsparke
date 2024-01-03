0d331b2a1de6d699268f41e2ab4d19508baa5ebb
Rajatsparke <96182123+Rajatsparke@users.noreply.github.com>
2024-01-03 04:56:45 +0000

    fbwbwb

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ for i in range(1, month + 1):
 
 total_salary_float = float(total_salary)
 # Calculate ROI
-roiii_mba = (total_salary / corresponding_value)  * 100
+roiii_mba = int(total_salary / corresponding_value)  * 100
 
 # Print results
 #st.write(f"Total salary earned after {month} months: {total_salary:.2f}")
@@ -108,7 +108,7 @@ for i in range(1, month_basl + 1):
 
 total_salary_float_basl = float(total_salary_basl)
 # Calculate ROI
-roiii_basl = (total_salary_basl / basl_investment)  * 100
+roiii_basl = int(total_salary_basl / basl_investment)  * 100
 
 # Print results
 #st.write(f"Total salary earned after {month} months: {total_salary:.2f}")
@@ -117,3 +117,7 @@ roiii_basl = (total_salary_basl / basl_investment)  * 100
 #st.write("Total earnings BASL",total_salary_float_basl)
 #st.write("ROI BASL",round(float(total_salary_float_basl/basl_investment)*100),"%")
 st.write("When you enroll yourself into the BLP program instead, here's how your earnings & ROI (over the same duration) would look like:")
+st.write("Earnings in",month,"months",int(total_salary_float_basl))
+st.write("ROI of the BLP Program :",int(total_salary_float_basl/basl_investment)*100)
+
+st.write('Over',month, 'months, the ROI of the BLP program is',roiii_basl-roiii_mba,'% more than the ROI of',selected_option)
