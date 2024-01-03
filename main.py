22d297c6f1c29dc0e4ea425ed99010df45ce02e3
Rajatsparke <96182123+Rajatsparke@users.noreply.github.com>
2024-01-03 03:09:47 +0000

    removed add code

--- main.py
+++ main.py
@@ -58,77 +58,7 @@ mba_salary = round(float(salary))
 #n = st.slider("Years from today",0,5,1)
 month = st.slider(f"Months since placement from {selected_option}",1,60,1)
 #a = st.slider("Months since placement",-6,30,1)
-'''a = slider + 18
 
-#if n == 0:
- #   st.write("Your approx. salary per month will be", basl_salary/12)
- #   st.write("Therefore, ROI after 6 months from today will be",round(((basl_salary/12)/basl_investment)*100),'%')
-b = 0
-c = basl_salary/12*(12)
-
-e = (c+((basl_salary*(1+annual_growth/100))/12*(a%12)))
-roi_basl = 0
-roi_mba = 0
-if 1<=a<13:
-    b = basl_salary/12*(a%13)
-    st.write(" *Opted for BASL:* Congratulations on your placement! \n Your have approx. earned Rs.", basl_salary/12*(a%13))
-    
-    roi_basl = (round((b/basl_investment)*100))
-    st.write(" *Opted for MBA :* You're still in training")
-    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round((b/basl_investment)*100)),'% vs 0')
-elif a <  1:
-    st.write("You are in training")
-    roi = 0
-elif 13<=a<=18:
-    d = (basl_salary*(1+annual_growth/100))/12
-    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is Rs.",round(basl_monthly_salary*(1+annual_growth/100)))
-    st.write("Your total earnings after",a ,'months via BASL is',(c+(d*(a%12))))
-    e = (c+(d*(a%12)))
-    st.write(" *Opted for MBA :* You're still in training")
-    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round(((c+(d*(a%12)))/basl_investment)*100)),'% vs 0')
-    roi_basl = (round(((c+(d*(a%12)))/basl_investment)*100))
-   
-elif 18<a<24:
-    d = (basl_salary*(1+annual_growth/100))/12
-    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is Rs.",round(basl_monthly_salary*(1+annual_growth/100)))
-    st.write("Your total earnings after",a ,'months via BASL is',round(c+(d*(a%12))))
-    e = (c+(d*(a%12)))
-    roi_basl = (round(((c+(d*(a%12)))/basl_investment)*100))
-    st.write("You got placed via MBA and your monthly salary is", round(mba_salary/12))
-    st.write("You total earnings till now via an MBA are",round((mba_salary/12)*(a%18)))
-    roi_mba = ((((mba_salary/12)*(a%18))/corresponding_value)*100)
-    roii_mba = round(float(roi_mba))
-    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round(((c+(d*(a%12)))/basl_investment)*100)),'% vs',roii_mba,'%')
-    comparison = round(roi_basl/roii_mba)
-    st.markdown(f"BASL ROI is :orange[{comparison}] times {selected_option}'s")
-elif a==24:
-    d = (basl_salary*(1+annual_growth/100))/12
-    st.write("You received a",annual_growth,"% increment and hence your new monthly salary is Rs.",round(basl_monthly_salary*(1+annual_growth/100)))
-    st.write("Your total earnings after",slider ,'months via BASL is',round((c+(d*(12)))))
-    
-    roi_basl = (round(((c+(d*(12)))/basl_investment)*100))
-    st.write("You got placed via MBA and your monthly salary is",round(mba_salary/12))
-    st.write("You total earnings till now via an MBA are", round((mba_salary/12)*(a%18)))
-    roi_mba = ((((mba_salary/12)*(a%18))/corresponding_value)*100)
-    roii_mba = round(float(roi_mba))
-    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',(round(((c+(d*(12)))/basl_investment)*100)),'% vs',roii_mba,'%')
-elif 25<=a<=30:
-    d = (basl_salary*(1+annual_growth/100))/12
-    st.write("You received a",annual_growth,"% increment again and hence your new monthly salary is Rs.",round(d*(1+annual_growth/100)))
-    f = (d*(1+annual_growth/100))
-    x = round((c+(d*(12))))
-    st.write("Your total earnings after",a ,'months via BASL is',round(x+(f*(a%12))))
-    
-    roi_basl = ((x+(f*(a%12))/basl_investment)*100)
-    st.write("You got placed via MBA and your monthly salary is", round(mba_salary/12))
-    st.write("You total earnings till now via an MBA are",round((mba_salary/12)*(a%18)))
-    roi_mba = ((((mba_salary/12)*(a%18))/corresponding_value)*100)
-    roii_mba = round(float(roi_mba))
-    st.write(" **ROI of BASL vs ROI of MBA upto month** ",a,':',round(((x+(f*(a%12)))/basl_investment)*100),'% vs',roii_mba,'%')
-
-
-
-st.link_button("Apply Now ","https://www.kraftshala.com/business-management-course/")'''
 
 # Validate month input
 if not 1 <= month <= 60:
