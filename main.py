6415fcffd248ef5d85a30639c4055f0637c6061b
Rajatsparke <96182123+Rajatsparke@users.noreply.github.com>
2024-01-03 04:49:12 +0000

    inttttttt

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ roiii_mba = (total_salary / corresponding_value)  * 100
 #st.write(f"Total salary earned after {month} months: {total_salary:.2f}")
 #st.write(f"ROI after {month} months: {roi:.2f}%")
 
-st.write("Earnings in",month,"months :",total_salary_float)
+st.write("Earnings in",month,"months :",int(total_salary_float))
 st.write("ROI of studying at",selected_option,":",(int(total_salary_float/corresponding_value)*100),"%")
 
 
